3372aabd5cac5b3244153529ac0b2c8f53cf2e56
Demod <demodulent@gmail.com>
2024-09-20 01:43:48 -0400

    SpriteLayer render (not tested)

--- thumbyRetro.py
+++ thumbyRetro.py
@@ -61,13 +61,16 @@ class Bitmap:
         self.tileHeight = self.height // self.rows
 
 class Sprite:
-    def __init__(self, bitmap, x=0, y=0, frame=0, mirrorX=0, mirrorY=0):
+    def __init__(self, bitmap, x=0, y=0, frame=0):
         self.bitmap = bitmap
         self.x = x
         self.y = y
+        self.width = bitmap.tileWidth
+        self.height = bitmap.tileHeight
         self.frame = frame
-        self.mirrorX = mirrorX
-        self.mirrorY = mirrorY
+        # TODO
+        # self.mirrorX = mirrorX
+        # self.mirrorY = mirrorY
 
 class BitmapLayer:
     def __init__(self, bitmap=None, x=0, y=0, frame=0):
@@ -77,7 +80,7 @@ class BitmapLayer:
         self.frame = frame
         
     @micropython.viper
-    def render(buf:ptr16, offset:int, x:int, y:int, w:int):
+    def render(self, buf:ptr16, offset:int, x:int, y:int, w:int):
         pass
 
 class TileLayer:
@@ -93,18 +96,101 @@ class TileLayer:
         self.y = 0
         
     @micropython.viper
-    def render(buf:ptr16, offset:int, x:int, y:int, w:int):
+    def render(self, buf:ptr16, offset:int, x:int, y:int, w:int):
         pass
+
+_SL_CACHE_COUNT = const(0)
+_SL_CACHE_YSTOP = const(1)
+_SL_CACHE_XSTOP = const(2)
+_SL_CACHE_MINSIZE = const(16)
 
 class SpriteLayer:
     def __init__(self):
         self.sprites = []
+
+        self._cache = bytearray(3 + _SL_CACHE_MINSIZE)
+        self._cacheCount = 0
+        self._cacheYStop = -1
         
     @micropython.viper
-    def render(buf:ptr16, offset:int, x:int, y:int, w:int):
-        pass
-        
+    def invalidate(self):
+        cache = ptr8(self._cache)
+        cache[_SL_CACHE_YSTOP] = -1
 
+    @micropython.viper
+    def render(self, buf:ptr16, offset:int, x:int, y:int, w:int):
+        cache = ptr8(self._cache)
+        sprites = ptr(self.sprites)
+        if cache[_SL_CACHE_YSTOP] < y or cache[_SL_CACHE_XSTOP] < x:
+            minY2 = HEIGHT-1
+            nextY1 = HEIGHT-1
+            ci = 3 + cache[_SL_CACHE_COUNT]
+            for i in range(int(len(self.sprites))):
+                sprite = sprites[i]
+                x1 = int(sprite.x)
+                y1 = int(sprite.y)
+                x2 = sprX1 + int(sprite.width)
+                y2 = sprY1 + int(sprite.height)
+                if y1 > y:
+                    nextY1 = min(nextY1, y1)
+                if x1 > (x+w) or x2 <= x or y1 > y or y2 <= y:
+                    continue
+                minY2 = min(minY2, y2)
+                cache[ci] = i
+                ci += 1
+            count = ci - 3
+            cache[_SL_CACHE_COUNT] = count
+            if count > 0:
+                cache[_SL_CACHE_YSTOP] = minY2 - 1
+                cache[_SL_CACHE_XSTOP] = x + w - 1
+            else:
+                cache[_SL_CACHE_YSTOP] = nextY1 - 1
+                cache[_SL_CACHE_XSTOP] = WIDTH
+        else:
+            count = cache[_SL_CACHE_COUNT]
+
+        buf = ptr16(PPU.buf)
+        for ci in range(3,count+3):
+            sprite = cache[ci]
+            sprW = int(sprite.width)
+            sprH = int(sprite.height)
+            bitmap = sprite.bitmap
+            bmpW = int(bitmap.width)
+            bmp = ptr16(bitmap.data)
+            key = int(bitmap.key) 
+            frame = int(sprite.frame)
+
+            srcY = y - int(sprite.y)
+            if frame > 0:
+                cols = bmpW // sprW
+                row = frame // cols
+                col = frame % cols
+                srcX = col * sprW
+                srcY += row * sprH
+            else:
+                srcX = 0
+
+            dstX = int(sprite.x)
+            w = sprW
+            
+            if dstX < 0:
+                srcX -= dstX
+                w += dstX
+                dstX = 0
+            if (dstX+w) >= WIDTH:
+                w -= (dstX+w) - WIDTH
+
+            si = srcY * bmpW + srcX
+            di = y * WIDTH + dstX
+            for _ in range(w):
+                c = bmp[si] & 0xFFFF
+                if c != key:
+                    buf[di] = c
+                si += dx
+                di += 1
+
+        
+# TODO deferred rendering techniques?
 # class ScanLine:
 #     def __init__(self):
 #         self.pixels = array.array("H",[Color.BLACK for _ in range(WIDTH)])
