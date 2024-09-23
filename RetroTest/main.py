a73e3197138f454e98258cd32d0a9758b8253f2e
Demod <demodulent@gmail.com>
2024-09-23 15:01:11 -0400

    Changed bitmap test to use larger bitmaps

--- RetroTest/main.py
+++ RetroTest/main.py
@@ -22,38 +22,60 @@ from thumbyRetro import *
 
 import math
 
+bmpCastle = Bitmap("castle.bmp", Color.MAGENTA)
+bmpCastleBG = Bitmap("castle_bg.bmp")
 bmpTile = Bitmap("tile.bmp")
 
 #
 #   Test Bitmap Layer
 #
 def test_bitmapLayer():
-    layerBG = BitmapLayer(bmpTile)
+    layerSky = BitmapLayer(bmpCastleBG)
+    layerBG = BitmapLayer(bmpCastle)
     PPU.layers = [
+        layerSky,
         layerBG
     ]
 
-    x = 0
+    skyX = 0
+    bgX = 0
     t = 0
+    intensity = 2
     while True:
         # Next test
-        if BtnMENU.is_just_pressed:
-            break
+        dTest = 1 if BtnRB.is_just_pressed else -1 if BtnLB.is_just_pressed else 0
+        if dTest:
+            return dTest
+
+        dx = 1 if BtnR.is_pressed else -1 if BtnL.is_pressed else 0
+        dint = 0.1 if BtnU.is_pressed else -0.1 if BtnD.is_pressed else 0
         
-        x -= 0.75
-        layerBG.y += 1
-        t += 0.1
+        bgX = max(0,min(layerBG.bitmap.width-WIDTH,bgX+dx))
+        intensity = max(0,min(5,intensity+dint))
+                    
+        layerBG.x = -bgX
+        skyX -= 0.25 * intensity
+        t += 0.01 * intensity
+        r = 5 * intensity
         for i in range(HEIGHT):
-            layerBG.x = int(x+5*math.sin(t+i/20))
+            layerSky.x = int(skyX-bgX+r*math.sin(t+i/10))
             PPU.scanLine(i)
         PPU.update()
         
+#
+#   Test Sprite Layer
+#
+def test_spriteLayer():
+    return 1 # TODO
+        
 tests = [
-    test_bitmapLayer
+    test_bitmapLayer,
+    test_spriteLayer
 ]        
 
 PPU.setFPS(30)
+
+testIndex = 0
 while True:
-    for test in tests:
-        test()
-        PPU.update()
+    testIndex = (testIndex + tests[testIndex]() + len(tests)) % len(tests)
+    PPU.update()
